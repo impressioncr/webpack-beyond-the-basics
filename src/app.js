5fae86945c1d9b6a44260bbcd646d4712725ed86
impressioncr <impressioncr@hotmail.com>
2018-09-09 13:42:16 +0800

    react-final

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(
+  <div>
+    Hello, from react!
+  </div>,
+  document.getElementById("react-root")
+)
--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ require('webpack-hot-middleware/client?reload=true')
 require('./main.css')
 require('./images/link.jpg')
 require('./index.html')
-
-const a = async () => {
-  await console.log('Hello from the future!')
-}
+require("./app")
